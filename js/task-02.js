75665daab46084ae9f2a44651e092d6109da2da1
Olha Maistrenko <olga.maistrenko@outlook.com>
2023-03-18 16:32:05 +0100

    task02
    task02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const id = document.querySelector('#ingredients');
+console.log(id);
+
+
+
+for (let i = 1; i <= 5; i += 1) {
+  const createLi = document.createElement('li');
+  console.log(createLi)
+  createLi.innerHTML = ingredients[i];
+  createLi.classList.add("item");
+  id.appendChild(createLi)
+}
