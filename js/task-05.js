c14f7c55c71bb8cd3c57547ace938b77eefabc8d
Olha Maistrenko <olga.maistrenko@outlook.com>
2023-03-20 22:21:10 +0100

    task05
    task05

---
+++ js/task-05.js
@@ -0,0 +1,12 @@
+
+
+
+const inputEl = document.querySelector('#name-input');
+const outputEl = document.querySelector('#name-output');
+
+inputEl.addEventListener("input", (inputText) => {
+    if (inputEl.value === '') {
+    return outputEl.textContent = "Anonymous";
+}
+    outputEl.textContent = inputText.currentTarget.value;
+});
