585158784ff6f36f3865292d357394fc1da61ded
Olha Maistrenko <olga.maistrenko@outlook.com>
2023-03-21 19:48:59 +0100

    task07
    task07

--- js/task-07.js
+++ js/task-07.js
@@ -1,17 +1,12 @@
-// Напиши скрипт, який реагує на зміну значення input#font - size - control
-// (подія input) і змінює інлайн - стиль span#text, оновлюючи властивість
-// font - size.В результаті, перетягуючи повзунок, буде змінюватися розмір тексту.
 
 const input = document.querySelector('#font-size-control');
-console.log(input);
 const inputText = document.querySelector('#text');
-console.log(inputText);
-let step = 0;
+
 
 input.addEventListener('input', movePoint);
 
-function movePoint(event) {
-    step += 1;
-    event.target.style.fontSize = step + 'px';
-   
+function movePoint() {
+    inputText.style.fontSize = input.value + 'px';
+    console.log(inputText.style.fontSize);
 }
+
--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,3 @@
-// Напиши скрипт, який змінює кольори фону елемента < body > через інлайн - стиль 
-// по кліку на button.change - color і виводить значення кольору в span.color.
 
 
 const body = document.querySelector('body');
