6fa859dbcb1ff8cfacd7693c41e60bf365c4732c
Olha Maistrenko <olga.maistrenko@outlook.com>
2023-03-21 16:56:02 +0100

    task09
    task09

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,23 @@
-function getRandomHexColor() {
+// Напиши скрипт, який змінює кольори фону елемента < body > через інлайн - стиль 
+// по кліку на button.change - color і виводить значення кольору в span.color.
+
+
+const body = document.querySelector('body');
+const changeColorEl = document.querySelector('.change-color')
+const backgroundColor = document.querySelector('.color')
+
+
+changeColorEl.addEventListener('click', changeBackgroundColor);
+
+function changeBackgroundColor() {
+  const changeColor = getRandomHexColor();
+
+  body.style.backgroundColor = changeColor;
+  backgroundColor.textContent = changeColor;
+    
+      function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
 }
