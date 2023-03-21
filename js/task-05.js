54f562f19d1fd124463bf0c15854281788945a6b
Olha Maistrenko <olga.maistrenko@outlook.com>
2023-03-21 13:42:03 +0100

    task06
    task06

--- js/task-05.js
+++ js/task-05.js
@@ -1,4 +1,4 @@
-
+ 
 
 
 const inputEl = document.querySelector('#name-input');
---
+++ js/task-06.js
@@ -0,0 +1,19 @@
+// Яка кількість символів повинна бути в інпуті, зазначається
+// в його атрибуті data - length.
+// Якщо введена правильна кількість символів, то border інпуту стає зеленим,
+//  якщо неправильна кількість - червоним.
+
+const textInput = document.querySelector('input');
+const textLength = textInput.getAttribute("data-length");
+
+
+textInput.addEventListener("blur", event => {
+    if (event.target.value.length === Number(textLength)) {
+    textInput.classList.add('valid');
+    textInput.classList.remove('invalid');
+    } else {
+        textInput.classList.add('invalid');
+        textInput.classList.remove('valid');
+}
+});
+
