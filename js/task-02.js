bf57c36fde4e7c6235128ca8eef18adde5850127
Olha Maistrenko <olga.maistrenko@outlook.com>
2023-03-24 16:30:42 +0100

    task02
    task 02 with map()

--- js/task-02.js
+++ js/task-02.js
@@ -8,14 +8,26 @@ const ingredients = [
 ];
 
 const id = document.querySelector('#ingredients');
-console.log(id);
 
 
-
-for (let i = 1; i <= 5; i += 1) {
+const listItem = ingredients.map((ingredient) => {
   const createLi = document.createElement('li');
-  console.log(createLi)
-  createLi.innerHTML = ingredients[i];
-  createLi.classList.add("item");
-  id.appendChild(createLi)
-}
+  console.log(createLi);
+  createLi.textContent = ingredient;
+  createLi.classList.add('item');
+  return createLi;
+})
+
+id.append(...listItem)
+
+
+
+
+
+// for (let i = 1; i <= 5; i += 1) {
+//   const createLi = document.createElement('li');
+//   console.log(createLi)
+//   createLi.innerHTML = ingredients[i];
+//   createLi.classList.add("item");
+//   id.appendChild(createLi); 
+// }
